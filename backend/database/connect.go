d8bccc10590a710656317595945c707cc7b44068
RaviAzzuraPutra <ravi.azzura74@gmail.com>
2025-10-22 14:23:39 +0700

    MENYESUAIKAN SEDIKIT

--- backend/database/connect.go
+++ backend/database/connect.go
@@ -5,6 +5,7 @@ import (
 	"backend/model"
 	"fmt"
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -16,25 +17,47 @@ func ConnectDB() {
 
 	var errorConnect error
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
-		db_config.DBConfig().DB_HOST, db_config.DBConfig().DB_USER, db_config.DBConfig().DB_PASSWORD, db_config.DBConfig().DB_NAME, db_config.DBConfig().DB_PORT, db_config.DBConfig().DB_SSLMODE, db_config.DBConfig().DB_TIMEZONE)
+	if os.Getenv("DEVELOPMENT") == "true" {
+		dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
+			db_config.DBConfig().DB_HOST, db_config.DBConfig().DB_USER, db_config.DBConfig().DB_PASSWORD, db_config.DBConfig().DB_NAME, db_config.DBConfig().DB_PORT, db_config.DBConfig().DB_SSLMODE, db_config.DBConfig().DB_TIMEZONE)
 
-	DB, errorConnect = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		DB, errorConnect = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
-	errMigrate := DB.AutoMigrate(&model.User{}, &model.Todo{}, &model.Session{})
+		errMigrate := DB.AutoMigrate(&model.User{}, &model.Todo{}, &model.Session{})
 
-	if errMigrate != nil {
-		log.Printf("GAGAL MIGRASI DATABASE ❌ " + errMigrate.Error())
+		if errMigrate != nil {
+			log.Printf("GAGAL MIGRASI DATABASE ❌ " + errMigrate.Error())
+		}
+
+		if errorConnect != nil {
+			panic("GAGAL TERHUBUNG KE DATABASE" + errorConnect.Error())
+		}
+
+		if DB == nil {
+			panic("DATABASE TIDAK TERHUBUNG")
+		}
+
+		log.Printf("BERHASIL TERHUBUNG KE DATABASE ✅ Development Mode")
+
+	} else if os.Getenv("DEVELOPMENT") != "true" {
+		database_url := os.Getenv("DATABASE_URL")
+		DB, errorConnect = gorm.Open(postgres.Open(database_url), &gorm.Config{})
+
+		errMigrate := DB.AutoMigrate(&model.User{}, &model.Todo{}, &model.Session{})
+
+		if errMigrate != nil {
+			log.Printf("GAGAL MIGRASI DATABASE ❌ " + errMigrate.Error())
+		}
+
+		if errorConnect != nil {
+			panic("GAGAL TERHUBUNG KE DATABASE" + errorConnect.Error())
+		}
+
+		if DB == nil {
+			panic("DATABASE TIDAK TERHUBUNG")
+		}
+
+		log.Printf("BERHASIL TERHUBUNG KE DATABASE ✅ Production Mode")
 	}
-
-	if errorConnect != nil {
-		panic("GAGAL TERHUBUNG KE DATABASE" + errorConnect.Error())
-	}
-
-	if DB == nil {
-		panic("DATABASE TIDAK TERHUBUNG")
-	}
-
-	log.Printf("BERHASIL TERHUBUNG KE DATABASE ✅ Development Mode")
 
 }
