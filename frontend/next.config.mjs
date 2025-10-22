885b4430ee829c72d4f1a1e530733e52d4bcf4a6
RaviAzzuraPutra <ravi.azzura74@gmail.com>
2025-10-22 20:16:45 +0700

    MEMBENARKAN NEXT.CONFIG.MJS

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,12 +2,6 @@ import withPWA from "next-pwa";
 
 const nextConfig = {
     reactStrictMode: true,
-    images: {
-        domains: ["res.cloudinary.com"],
-    },
-    experimental: {
-        optimizeCss: true,
-    },
 };
 
 export default withPWA({
